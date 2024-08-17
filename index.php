9c2e3962d0109c2d46a0942bbe5c3fdf897453f0
Rival Gian <rivalgian20@gmail.com>
2024-08-17 18:24:06 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
 </head>
+
 <body>
     <h1>Hello</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis n
+        ostrum molestias quam earum ex? Necessitatibus aspernatur magni id fuga fugiat velit vel. 
+        Consequatur molestiae facilis blanditiis totam possimus ab molestias!</p>
 </body>
+
 </html>
